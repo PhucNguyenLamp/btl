be4a0b5be0843845f1a6c3af2cc5e82613be8f96
phuc <giaphuc29082004@gmail.com>
2023-02-23 23:54:29 +0700

    remedy + tiny

--- all.cpp
+++ all.cpp
@@ -21,31 +21,66 @@ void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int
 
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue) {
     //inital
-    int event ,i = 1; //event array
-    int MaxHP = HP;
+    int event ,i = 1, j=-1; //event array
     rescue = -1; //initial
     ifstream data;
     data.open("tc1_input");
     data >> HP >> level >>  remedy >> maidenkiss >> phoenixdown;
+    int MaxHP = HP;
 
     //processing
     while (data >> event){
+        
         if (event == 0) {
             rescue = 1;
             break;
         }
+
+        int b=i%10;
+        int levelO = i > 6?(b > 5?b : 5) : b;
+
         if (event == 1||event == 2||event ==3|| event ==4||event ==5){
-            int b=i%10;
-            int levelO = i > 6?(b > 5?b : 5) : b;
-            if (level == levelO) if (level <10) level++;
+            if (level > levelO){
+                if (level < 10) level++;
+            }
             else if (level < levelO) {
                 float baseDamage = event==1?1:event==2?1.5:event==3?4.5:event==4?7.5:9.5;
                 int damage = baseDamage * levelO * 10;
                 HP = HP - damage;
             }
 
+            if (event == 6){
+            if (level > levelO){
+                level+=2;
+                if (level>10) level=10;
+            }
+            else if (level < levelO) {
+                if (remedy>=1){
+                    remedy--;
+                    break;
+                }
+                HP/=5;
+                if (HP==0) HP = 1;
+                j=3;
+            }    
+            }
+
+            if (HP<=0 && phoenixdown<=0){
+                rescue = 0;
+                break;
+            }
+            else if (HP<=0 && phoenixdown>0){
+                phoenixdown--;
+                HP = MaxHP;
+            }
+
+        j--; if (j==0){
+            HP *=5;
+            if (HP > MaxHP) HP = MaxHP;
+        }   
         i++;
         }
+        rescue = 1;
     }
     //rescue = 
 
