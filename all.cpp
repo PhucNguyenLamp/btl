be05ead86802c47c49199049b673ffebd2fd4bcd
phuc <giaphuc29082004@gmail.com>
2023-02-24 00:46:01 +0700

    frog

--- all.cpp
+++ all.cpp
@@ -21,24 +21,24 @@ void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int
 
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue) {
     //inital
-    int event ,i = 1, j=-1; //event array
-    rescue = -1; //initial
+    int event ,i = 1, j=-1, k=-1; //event array
+    rescue = 1; //initial
     ifstream data;
     data.open("tc1_input");
     data >> HP >> level >>  remedy >> maidenkiss >> phoenixdown;
     int MaxHP = HP;
+    int olevel = level;
 
     //processing
     while (data >> event){
-        
+        //win
         if (event == 0) {
-            rescue = 1;
             break;
         }
-
+        //level quai
         int b=i%10;
         int levelO = i > 6?(b > 5?b : 5) : b;
-
+        //quai quen
         if (event == 1||event == 2||event ==3|| event ==4||event ==5){
             if (level > levelO){
                 if (level < 10) level++;
@@ -48,44 +48,66 @@ void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, in
                 int damage = baseDamage * levelO * 10;
                 HP = HP - damage;
             }
+        }
+        //phat^.
+        if (event == 6 && j<=0&&k<=0){
+        if (level > levelO){
+            level+=2;
+            if (level>10) level=10;
+        }
+        else if (level < levelO) {
+            if (remedy>=1){
+                remedy--;
+            } else {
+            HP/=5;
+            if (HP==0) HP = 1;
+            j=4; //vi qua luot nay la con 3
+            }
+        }    
+        }
+        //con coc
+        if (event == 7&&j<=0&&k<=0){
+        if (level > levelO){
+            level+=2;
+            if (level>10) level=10;
+        }
+        else if (level < levelO) {
+            if (maidenkiss>=1){
+                remedy--;
+            } else {
+            level=1;
+            k=4;
+            }
+        }    
+        }
+        //die hay ko die
+        if (HP<=0 && phoenixdown<=0){
+            rescue = 0;
+            break;
+        }
+        else if (HP<=0 && phoenixdown>0){
+            phoenixdown--;
+            HP = MaxHP;
+        }
 
-            if (event == 6){
-            if (level > levelO){
-                level+=2;
-                if (level>10) level=10;
-            }
-            else if (level < levelO) {
-                if (remedy>=1){
-                    remedy--;
-                    break;
-                }
-                HP/=5;
-                if (HP==0) HP = 1;
-                j=3;
-            }    
-            }
+        //nhat do giua duong
 
-            if (HP<=0 && phoenixdown<=0){
-                rescue = 0;
-                break;
-            }
-            else if (HP<=0 && phoenixdown>0){
-                phoenixdown--;
-                HP = MaxHP;
-            }
-
+        //postprocessing
         j--; if (j==0){
             HP *=5;
             if (HP > MaxHP) HP = MaxHP;
         }   
+
+        k--; if (k==0){
+            level=olevel;
+        }   
+
         i++;
-        }
-        rescue = 1;
+    
     }
-    //rescue = 
 
     //displaying
-    end:display(HP, level, remedy, maidenkiss, phoenixdown, rescue); 
+    display(HP, level, remedy, maidenkiss, phoenixdown, rescue); 
 
 }
 
